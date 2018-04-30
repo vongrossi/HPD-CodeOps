af80e85bc0b0dec2bcbe05d974b4093ad6eb0ec5
vongrossi <vongrossi@gmail.com>
2018-04-30 19:17:59 -0300

    aula 3 python

--- python/docker-cli-v2.py
+++ python/docker-cli-v2.py
@@ -13,7 +13,8 @@ def logando(mensagem, e, logfile='docker-cli.log'):
 def criar_container(args):
     try:
         client = docker.from_env()
-        executando = client.containers.run(args.imagem, args.comando)
+        executando = client.containers.run(args.imagem, args.comando, detach=True)
+        print("O container %s foi criado com sucesso" % executando.short_id)
         return(executando)
     except docker.errors.ImageNotFound as e:
         logando('Erro: Essa imagem não existe!', e)
@@ -32,6 +33,15 @@ def listar(args):
         conectando = client.containers.get(cada_container.id)
         print("O container %s utiliza a imagem %s e esta rodando o comando %s" % (conectando.short_id, conectando.attrs['Config']['Image'], conectando.attrs['Config']['Cmd']))
 
+def procurar_container(args):
+    client = docker.from_env()
+    get_all = client.containers.list()
+    for cada_container in get_all:
+        conectando = client.containers.get(cada_container.id)
+        imagem_container = conectando.attrs['Config']['Image']
+        if str(args.imagem).lower() in str(imagem_container).lower():
+            print("Achei o container %s que contem a palavra %s no nome de sua imagem: %s" % (cada_container.short_id, args.imagem, imagem_container))
+
 parser = argparse.ArgumentParser(description="Meu CLI docker fodao feito durante a aula do HPD.")
 subparser = parser.add_subparsers()
 
@@ -42,6 +52,10 @@ criar_opt.set_defaults(func=criar_container)
 
 listar_opt = subparser.add_parser('listar')
 listar_opt.set_defaults(func=listar)
+
+procurar_opt = subparser.add_parser('procurar')
+procurar_opt.add_argument('--imagem', required=False)
+procurar_opt.set_defaults(func=procurar_container)
 
 cmd = parser.parse_args()
 cmd.func(cmd)
